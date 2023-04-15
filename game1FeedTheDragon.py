5b6334abdbc31b612cd85c2ae756c7605f3aa193
sereno <sereno.project@gmail.com>
2023-04-15 12:20:44 +0330

    finishing the game

--- game1FeedTheDragon.py
+++ game1FeedTheDragon.py
@@ -20,7 +20,7 @@ clock = pygame.time.Clock()
 
 #set game values
 PLAYER_VELOCITY = 5
-PLAYER_STARTER_LIVES = 5
+PLAYER_STARTER_LIVES = 1
 COIN_STARTTIGN_VELOCITY = 5
 COIN_ACCELERATION = 0.5  # coin will speed up
 BUFFER_DISTANCE = 100 # while it goes off the screen
@@ -39,15 +39,15 @@ BLACK = (0,0,0)
 font = pygame.font.Font('AttackGraffiti.ttf', 32)
 
 #set text 
-score_text = font.render("Score: "+str(score), True, GREEN, DARKGREEN)
+score_text = font.render("Score: "+str(score), True, GREEN, BLACK)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10,10)
 
-player_lives_text = font.render("Lives: "+str(player_lives), True, GREEN, DARKGREEN)
+player_lives_text = font.render("Lives: "+str(player_lives), True, GREEN, BLACK)
 player_lives_rect = player_lives_text.get_rect()
 player_lives_rect.topright = (WINDOW_WIDTH-10,10)
 
-title_text = font.render("Feed the dragon", True, GREEN, WHITE)
+title_text = font.render("Feed the dragon", True, GREEN, BLACK)
 title_rect = title_text.get_rect()
 title_rect.centerx = WINDOW_WIDTH//2
 title_rect.y = 10
@@ -79,6 +79,7 @@ pygame.mixer.music.load("ftd_background_music.wav")
 
 
 #the main game loop
+pygame.mixer.music.play(-1,0.0)
 running = True
 while running:
     #lopp through a list of ecents 
@@ -87,25 +88,67 @@ while running:
         if event.type == pygame.QUIT:
            running = False 
     
- 
+     
     
     #get the list of keys pressed on keyboared
     keys = pygame.key.get_pressed()
        
-    #move the dragon continiously
-    # if keys[pygame.K_UP] or keys[pygame.K_w] and dragon_rect.top > 0:
-    #     dragon_rect.y -= VELOCITY
-    # if keys[pygame.K_DOWN]or keys[pygame.K_s] and dragon_rect.bottom < WINDOW_HEIGHT:
-    #     dragon_rect.y += VELOCITY  
-    # if keys[pygame.K_RIGHT] or keys[pygame.K_d] and dragon_rect.right < WINDOW_WIDTH:
-    #     dragon_rect.x += VELOCITY  
-    # if keys[pygame.K_LEFT] or keys[pygame.K_a] and dragon_rect.left > 0:
-    #     dragon_rect.x -= VELOCITY     
-
-
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and player_rect.y > 64 :
+        player_rect.y -= PLAYER_VELOCITY
+       
+    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and player_rect.y < WINDOW_HEIGHT - 32:
+        player_rect.y += PLAYER_VELOCITY  
+ 
+   
+    #move the coin
+    if coin_rect.x < 0:
+        #player missed teh coin
+        player_lives -= 1
+        miss_sound.play()
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+    else:
+        #move the coin
+        coin_rect.x -= coin_velocity
+        
     #check for collison between two rects
+    if player_rect.colliderect(coin_rect):
+        score += 1
+        coin_sound.play()
+        coin_velocity += COIN_ACCELERATION
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+        
     
-    
+    #update text for score and lives
+    score_text = font.render("Score: "+str(score), True, GREEN, BLACK)
+    player_lives_text = font.render("Lives: "+str(player_lives), True, GREEN, BLACK)
+
+    #check for game over 
+    if player_lives == 0:
+            display_surface.blit(game_over_text, game_over_rect)
+            display_surface.blit(continue_text, continue_rect)
+            pygame.display.update()
+            
+            #pause the game until player presses a key and reset the game
+            pygame.mixer.music.stop()
+            is_paused = True
+            while is_paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        score = 0
+                        player_lives = PLAYER_STARTER_LIVES
+                        player_rect.y = WINDOW_HEIGHT//2
+                        coin_velocity = COIN_STARTTIGN_VELOCITY
+                        pygame.mixer.music.play(-1, 0.0)
+                        is_paused = False
+                    #the player wants to quit
+                    if event.type == pygame.QUIT:
+                        is_paused = False
+                        running = False 
+
+
+        
      #fill the display to cover the old images
     display_surface.fill(BLACK)   
     
